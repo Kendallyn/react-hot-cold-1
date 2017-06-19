4b117964fe8f02ef35ae2413e11791e304b18302
Kendallyn <Kendallynlyon@hotmail.com>
2017-06-19 11:29:47 -0700

    created redux files

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+//List everything user can do
+    //user can guess a number
+    //user can see hot or cold warning
+    //user can see previous numbers guessed
+    //user can make a different guess from before
