a0cf3caeef9c084b5ca8a610ce5912342dca02d7
Kendallyn <Kendallynlyon@hotmail.com>
2017-06-19 15:30:55 -0700

    added export const

--- src/actions.js
+++ src/actions.js
@@ -3,3 +3,20 @@
     //user can see hot or cold warning
     //user can see previous numbers guessed
     //user can make a different guess from before
+
+export const NEW_GAME = 'NEW_GAME';
+export const newGame = () => ({
+    type: NEW_GAME,
+    correctAnswer: Math.round(Math.random() * 100),
+});
+
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const makeGuess = (guess) => ({
+    type: MAKE_GUESS,
+    guess
+});
+
+export const TOGGLE_INFO_MODAL = 'TOGGLE_INFO_MODAL';
+export const toggleInfoModal = () => ({
+    type: TOGGLE_INFO_MODAL
+});
